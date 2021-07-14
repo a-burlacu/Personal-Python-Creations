5ceedd085e705d1c6964994aedb08ee1bad76ce9
msalina007 <79892093+msalina007@users.noreply.github.com>
2021-07-14 13:23:34 -0600

    Update auto exit program.py

--- auto exit program.py
+++ auto exit program.py
@@ -8,72 +8,20 @@ from datetime import datetime
 os.getcwd()
 print (os.getcwd())
 
-# Change the current directory to ("<path>") and show that it was done in console
+# Change the current directory to ("<your path>") and show that it was done in console
 os.chdir("C:/Users/Alina/AppData/Roaming/Zoom/bin")
 os.getcwd()
 print (os.getcwd())
 
-# After you run the program, wait until a specified date and time to execute following function
 
+# After you run the program, wait until a specified date and time to execute following function
 # syntax >> datetime(year,month,day,24hr,min)
 
 time= datetime(2021,7,14,12,58)
 pause.until(time)
 
 # Force close any program
+
 os.system("TASKKILL /F /IM Zoom.exe")
-
-
-
 print ("\nThe program was closed at:",time)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-shit that didn't work:
-
-"""
-#‪C:\Users\Alina\AppData\Roaming\Zoom\bin\Zoom.exe
-# def open_program (path_name):
-   
-#     return subprocess.Popen(path_name)
-
-#    #return subprocess.Popen(["cmd", "/c", "dir", "Roaming/Zoom/bin/Zoom.exe"])
-
-# def close_program(p):
-#     p.terminate()
-# path = '/Alina/AppData/Roaming/Zoom/bin/Zoom.exe'    
-    
-# path_name = ["cmd", "/c", "dir", "AppData/Roaming/Zoom/bin/Zoom.exe"]
-# open_program(path_name)
-# p=open_program(path_name)
-# time.sleep(3)
-# close_program(p)
