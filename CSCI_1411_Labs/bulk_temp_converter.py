a26afb20b271eaed749dd4c3b853e985c94e0cf9
msalina007 <79892093+msalina007@users.noreply.github.com>
2021-11-04 00:29:49 -0600

    updated prob stmt & task

--- CSCI_1411_Labs/bulk_temp_converter.py
+++ CSCI_1411_Labs/bulk_temp_converter.py
@@ -1,10 +1,13 @@
 """
-Task: Take a user input temperature in Fahrenheit, t, and convert to Celsius. 
-Display initial temp plus 10 following temps.
-
+Problem Statement:  
+You will convert all the temperatures from the initial value entered to the initial value entered plus 10 degrees. (For example, if the user enters 32 degrees, you will print out the conversion to celsius for 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, and 42 degrees.)
+Task:
+Create a program that does the following things:
+    1. Ask the user for the initial temperature in Fahrenheit.
+    2. Calculates the conversion of temperature to Celsius. 
+    3. Converts the range of temperatures from the initial to the inital + 10 degrees.
+    4. Prints the conversion for each of the temperatures. 
 """
-
-
 def temp_converter():
 
     t = int(input("What is the Fahrenheit temperature? "))
@@ -21,7 +24,4 @@ def temp_converter():
     # redefine t so the loop will move forward until it reaches t+11
         t += 1
 
-
 temp_converter()
-
-
